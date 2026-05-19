a9e3ffa3949492a0ac814680db6906c3e6f25787
MatthewKehoeErgo <matthew.kehoe@ergotechnologygroup.com>
2026-05-19 11:09:34 +0100

    Commit 11:09

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Dynamics_Test/",
+  base: "/Dynamics_Test_Matts_v01/",
   plugins: [react()],
 });
